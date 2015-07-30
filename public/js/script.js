0a7ccb3278256675844f9360680d762458a99c2d
Ali Waseem <waseema393@gmail.com>
2015-07-29 20:08:33 -0600

    added layout style and home cover to main site page

---
+++ public/js/script.js
@@ -0,0 +1,26 @@
+/**
+ * Created by awaseem on 15-07-27.
+ */
+
+$(document)
+    .ready(function() {
+
+        // fix menu when passed
+        $('.masthead')
+            .visibility({
+                once: false,
+                onBottomPassed: function() {
+                    $('.fixed.menu').transition('fade in');
+                },
+                onBottomPassedReverse: function() {
+                    $('.fixed.menu').transition('fade out');
+                }
+            });
+
+        // create sidebar and attach to menu open
+        $('.ui.sidebar')
+            .sidebar('attach events', '.toc.item');
+
+        $("#test").backstretch("http://semantic-ui.com/images/avatar/large/elliot.jpg");
+
+    });
