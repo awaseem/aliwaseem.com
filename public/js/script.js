37b0b91c6b1dee78c345b18700aea5d83011e631
Ali Waseem <waseema393@gmail.com>
2015-08-09 00:38:44 -0600

    added fade effect for homepage, images fade in after they have been loaded

--- public/js/script.js
+++ public/js/script.js
@@ -4,6 +4,21 @@
 
 $(document).ready(function() {
 
+    var selector = $(".ui.inverted.vertical.masthead.center.aligned.segment");
+    selector.waitForImages({
+        waitForAll: true,
+        finished: function () {
+            selector.animate({ opacity: 1 }, { duration: 1000 });
+        }
+    });
+
+    $(".proto-image").waitForImages({
+        waitForAll: true,
+        each: function () {
+            $(this).animate({ opacity: 1 }, { duration: 1500 });
+        }
+    });
+
     // fix menu when passed
     $('.masthead')
         .visibility({
@@ -23,5 +38,5 @@ $(document).ready(function() {
     $('.proto-image')
         .dimmer({
             on: 'hover'
-        })
+        });
 });
